05fb732a2151435edabcc0455ae78be1533524e2
john-forrest <londonjf@yahoo.co.uk>
2021-05-31 17:12:56 +0100

    Action sends a comment post message Mine from: https://gist.githubusercontent.com/weibeld/7a59df2b4482aa7b44e46b61c474feb8/raw/a46294949c7973de8dd1ccfeb8dcd98c8e3dd4d3/02-issue-greeter.yml

--- action-play/entrypoint.sh
+++ action-play/entrypoint.sh
@@ -1,3 +1,9 @@
-#!/bin/sh -l
+#!/usr/bin/env bash -l
 
-sh -c "echo Hello world - the test param is '${INPUT_TEST_PARAM}'"
+# Post comment to Issue #1 of john-forrest/actions-test-repo
+
+target_repo=john-forrest/actions-test-repo
+issue_number=1
+curl -s -X POST https://api.github.com/repos/${target_repo}/issues/$issue_number/comments \
+            -d '{"body":"Posted by $GITHUB_ACTOR from $GITHUB_REPOSITORY ($INPUT_TEST_PARAM)"}'
+            -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
