16d2119666da1119413453f2ce89148e996f6b11
john-forrest <londonjf@yahoo.co.uk>
2021-05-31 18:41:01 +0100

    Check repo_token is set

--- action-play/entrypoint.sh
+++ action-play/entrypoint.sh
@@ -2,6 +2,11 @@
 
 # Post comment to Issue #1 of john-forrest/actions-test-repo
 
+if [ -z "$REPO_TOKEN" ] ; then
+  echo "REPO_TOKEN is not set"
+  exit 1
+fi
+
 target_repo=john-forrest/actions-test-repo
 issue_number=1
 curl -s -X POST https://api.github.com/repos/${target_repo}/issues/$issue_number/comments \
