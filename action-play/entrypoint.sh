0fbea5e829f21fbf603345dba8e335541478291a
john-forrest <londonjf@yahoo.co.uk>
2021-05-31 18:50:30 +0100

    Fix curl post body - was not expanding variables Use double quotes consistently. Backslash for the ones in the message.

--- action-play/entrypoint.sh
+++ action-play/entrypoint.sh
@@ -10,5 +10,5 @@ fi
 target_repo=john-forrest/actions-test-repo
 issue_number=1
 curl -s -X POST https://api.github.com/repos/${target_repo}/issues/$issue_number/comments \
-            -d '{"body":"Posted by $GITHUB_ACTOR from $GITHUB_REPOSITORY ($INPUT_TEST_PARAM)"}' \
+            -d "{\"body\":\"Posted by $GITHUB_ACTOR from $GITHUB_REPOSITORY ($INPUT_TEST_PARAM)\"}" \
             -H "Authorization: Bearer $REPO_TOKEN"
